a012f0733083555099eb060a951b8288587e98bf
Vershinin <vershinin@starkovgrp.ru>
2025-02-17 14:13:29 +0500

    Обновление до 4.12

---
+++ starkov.EditableConstants/starkov.EditableConstants.ClientBase/ModuleBlockHandlers.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+
+namespace starkov.EditableConstants.Client.EditableConstantsBlocks
+{
+}
